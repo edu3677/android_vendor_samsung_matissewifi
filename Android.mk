2b629ef12ebb4e92129b1c7f3cb521a598c57b1c
sub77 <sub77@ymail.com>
2015-06-06 00:13:19 +0200

    typo

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter matisselte,$(TARGET_DEVICE)),)
+ifneq ($(filter matissewifi,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
